9244f5825e4eb0da1781fd183627b82a83bac6ae
Anthony Hillen <ah205@students.uwf.edu>
2019-01-07 16:06:51 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
 # code your #position_taken? method here!
+def position_taken?(board, index, player)
+  if board[index] == " " || ""
+    board[index] == player
+  else
+    return false
+  end
+end
